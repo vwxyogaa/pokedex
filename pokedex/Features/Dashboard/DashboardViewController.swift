feacd23a0e54ada1d56dc5d554fab4801a568d85
yxgg <yogajktf4@gmail.com>
2023-02-02 01:04:00 +0700

    dashboardController: config searchTextField | color navigationBar

--- pokedex/Features/Dashboard/DashboardViewController.swift
+++ pokedex/Features/Dashboard/DashboardViewController.swift
@@ -8,14 +8,31 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
-
+    @IBOutlet weak var searchPokemonTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViews()
+    }
+    
+    private func configureViews() {
         configureNavigationBar()
+        configureSearchPokemonTextField()
     }
     
     private func configureNavigationBar() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.title = "Pokedex"
+        self.navigationController?.navigationBar.standardAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        self.navigationController?.navigationBar.standardAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+    
+    private func configureSearchPokemonTextField() {
+        self.searchPokemonTextField.layer.cornerRadius = 10
+        self.searchPokemonTextField.layer.masksToBounds = true
+        searchPokemonTextField.attributedPlaceholder = NSAttributedString(
+            string: "Search",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray]
+        )
     }
 }
