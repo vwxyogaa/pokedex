e97abbc535f5341fa9a5b54b8249fd08842da069
Panji Yoga <panjiyogap@gmail.com>
2023-02-02 16:58:08 +0700

    configure start netfox

--- pokedex/Supporting Files/AppDelegate.swift
+++ pokedex/Supporting Files/AppDelegate.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import netfox
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    #if DEBUG
+        NFX.sharedInstance().start()
+    #endif
         return true
     }
 
