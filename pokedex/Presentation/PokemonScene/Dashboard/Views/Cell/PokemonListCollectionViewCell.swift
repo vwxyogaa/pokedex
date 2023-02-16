536d27bad6013d044bf6c06d93fe9b6ff140b74b
Panji Yoga <panjiyogap@gmail.com>
2023-02-16 15:54:16 +0700

    remove unused import

--- pokedex/Presentation/PokemonScene/Dashboard/Views/Cell/PokemonListCollectionViewCell.swift
+++ pokedex/Presentation/PokemonScene/Dashboard/Views/Cell/PokemonListCollectionViewCell.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Kingfisher
 
 class PokemonListCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var containerView: UIView!
--- pokedex/Presentation/PokemonScene/Detail/DetailPokemonViewController.swift
+++ pokedex/Presentation/PokemonScene/Detail/DetailPokemonViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Kingfisher
 import Lottie
 
 class DetailPokemonViewController: UIViewController {
