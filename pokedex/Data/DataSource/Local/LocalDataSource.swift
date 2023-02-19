df74896eda444d5ddae365246d994293a5d3570c
yxgg <yogajktf4@gmail.com>
2023-02-19 17:42:02 +0700

    LocalDataSource: fix error "Return expression of type '()' does not conform to 'Disposable"

--- pokedex/Data/DataSource/Local/LocalDataSource.swift
+++ pokedex/Data/DataSource/Local/LocalDataSource.swift
@@ -19,17 +19,17 @@ protocol LocalDataSourceProtocol {
 final class LocalDataSource: LocalDataSourceProtocol {
     func checkPokemonInCollection(pokemonId: Int) -> Observable<Bool> {
         return Observable<Bool>.create { observer in
-            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return Disposables.create() }
             let managedContext = appDelegate.persistentContainer.viewContext
             let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "MyCollection")
             do {
                 let result = try managedContext.fetch(fetchRequest)
                 for data in result as! [NSManagedObject] {
-                    guard let nickname = data.value(forKey: "nickname") as? String, let pokemonData = data.value(forKey: "pokemon") as? Data, let pokemon = Utils.getPokemonFromData(data: pokemonData) else { return }
+                    guard let pokemonData = data.value(forKey: "pokemon") as? Data, let pokemon = Utils.getPokemonFromData(data: pokemonData) else { return Disposables.create() }
                     if pokemon.id == pokemonId {
                         observer.onNext(true)
                         observer.onCompleted()
-                        return
+//                        return
                     }
                 }
                 observer.onNext(false)
@@ -43,9 +43,9 @@ final class LocalDataSource: LocalDataSourceProtocol {
     
     func catchPokemon(nickname: String, pokemon: Pokemon) -> Observable<Bool> {
         return Observable<Bool>.create { observer in
-            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return Disposables.create() }
             let managedContext = appDelegate.persistentContainer.viewContext
-            guard let myCollectionEntity = NSEntityDescription.entity(forEntityName: "MyCollection", in: managedContext) else { return }
+            guard let myCollectionEntity = NSEntityDescription.entity(forEntityName: "MyCollection", in: managedContext) else { return Disposables.create() }
             let myCollection = NSManagedObject(entity: myCollectionEntity, insertInto: managedContext)
             myCollection.setValue(nickname, forKey: "nickname")
             let pokemonData = Utils.getDataFromPokemon(pokemon: pokemon)
@@ -63,7 +63,7 @@ final class LocalDataSource: LocalDataSourceProtocol {
     
     func releasedPokemon(nickname: String) -> Observable<Bool> {
         return Observable<Bool>.create { observer in
-            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return Disposables.create() }
             let managedContex = appDelegate.persistentContainer.viewContext
             let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "MyCollection")
             fetchRequest.predicate = NSPredicate(format: "nickname == %@", nickname)
@@ -90,7 +90,7 @@ final class LocalDataSource: LocalDataSourceProtocol {
     func getMyCollections() -> Observable<[PokemonCollection]> {
         return Observable<[PokemonCollection]>.create { observer in
             var collections: [PokemonCollection] = []
-            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return Disposables.create() }
             let managedContext = appDelegate.persistentContainer.viewContext
             let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "MyCollection")
             do {
